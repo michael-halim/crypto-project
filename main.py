d780af9968523a62fe32250a63552582c7749717
Michael Halim <michaelhalimsenatra@gmail.com>
2021-10-24 22:55:33 +0700

    Added History and Top up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sha import sha256
+from datetime import datetime,date
 import os
 
 def MENU(opt='START_MENU'):
@@ -23,11 +24,34 @@ def login(file_name, username, hashed_pw):
 
     result = result[0].split()
     
-    if result[1] == hashed_pw:
+    if result[2] == hashed_pw:
         return 1
     
     return 0
 
+def fetchData(file_name,username):
+    line_number = 0
+    results = []
+
+    with open(file_name, 'r') as read_obj:
+
+        for line in read_obj:
+            line_number += 1
+
+            if username in line:
+                results.append((line.rstrip()))
+
+    results = [result.split('#') for result in results]
+    return results
+
+def prepareData(data):
+    tmp = data[1].split()
+    data = data[2:]
+    for val in data:
+        tmp.append(val)
+    
+
+    return tmp
 while True:
     chc = MENU()
     while True:
@@ -43,30 +67,77 @@ while True:
         
         if login(path,username, passwd):
             os.system('cls')
-
+            path = 'database/history.txt'
+            history_data = fetchData(path,username)
+            history_data = [prepareData(data) for data in history_data]
             while True:
-                chc = MENU('MAIN_MENU')
+                main_chc = MENU('MAIN_MENU')
 
-        
+                while True:
+                    if main_chc in ['1','2','3','0']:
+                        break
+                    print('Choose 1 / 2 / 3 / 0')
+                    main_chc = input('>>>')
+
+                if main_chc == '1':
+                    now = datetime.now()
+                    epoch = now.timestamp()
+                    historyid = sha256(str(epoch))[:11]
+
+                    _datetime = str(now.strftime('%Y-%m-%d %H:%M:%S'))
+                    bank = input('Input Bank : ')
+                    amount = input('Input Amount of Money : ')
+                    desc = 'Top Up From ' + bank
+                    sender = bank
+                    receiver = username
+                    _type = 'Top Up'
+                    string = historyid + '#' + _datetime + '#' + desc + '#' + _type + \
+                            '#' + amount + '#' + sender + '#' + receiver + '\n'
+                    f = open(path,'a')
+                    f.write(string)
+                    f.close()
+
+                elif main_chc == '2':
+                    print('Transfer')
+
+                elif main_chc == '3':
+                    print('Tanggal'.ljust(15) + 'Jam'.ljust(15) + 'Judul'.ljust(10) + 
+                    'Jenis Transfer'.ljust(20) + 'Nominal'.ljust(10) + 'Pengirim'.ljust(10) +'Penerima')
+                    for data in history_data:
+                        for value in data:
+                            print(value,end='  ')
+
+                        print()
+                
+                elif main_chc == '0':
+                    break
         else:
             print('Password atau Username Salah')
 
     if chc == '2':
-        username = input('Input Username : ')
-        password = input('Input Password : ')
         path = 'database/user.txt'
+        username = input('Input Username : ')
+        passwd = input('Input Password : ')
+        email = input('Input Email : ')
+        pin = input('Input PIN : ')
+        phone = input('Input Phone Number : ')     
+        now = datetime.now()
+        epoch = now.timestamp()
+        userid = sha256(str(epoch))[:11]
 
         if os.path.exists(path):
-            user = username + ' ' + sha256(password) + '\n'
+            user = userid + ' ' + username + ' ' + sha256(passwd) + ' ' + email + ' ' + pin + ' ' + phone + '\n'
             f = open(path,'a')
             f.write(user)
             f.close()
             
         else:
-            user = username + ' ' + sha256(password) + '\n'
+            user = userid + ' ' + username + ' ' + sha256(passwd) + ' ' + email + ' ' + pin + ' ' + phone + '\n'
             f = open(path,'a')
-            f.write('Username Password\n')
+            f.write('id username password email pin phone \n')
             f.write(user)
             f.close()
     if chc == '0':
         break
+
+    os.system('cls')
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-a = [('jkhadskjhasdhjk 123123312')]
-
-b = a[0].split()
+a = ['1', '2021-10-23 22:13:13', 'Transfer ke Halim', 'transfer', '300000', 'Yunus', 'Michael']
+b = a[1].split()
+a = a[2:]
+for val in a:
+    b.append(val)
+print(a)
 print(b)
