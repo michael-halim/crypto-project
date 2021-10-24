b22f278bdb5dc7e2dfe56d7114919001400b7e33
Michael Halim <michaelhalimsenatra@gmail.com>
2021-10-24 16:51:59 +0700

    added menu function and main menu

--- main.py
+++ main.py
@@ -1,49 +1,69 @@
 from sha import sha256
 import os
 
-def search_string_in_file(file_name, string_to_search):
+def MENU(opt='START_MENU'):
+    if opt == 'START_MENU':
+        print('1. Login\n2. Sign up\n0. Log Off \n')
+    elif opt == 'MAIN_MENU':
+        print('1. Top Up\n2. Transfer\n3. History\n0. Log Off \n')
+
+    return input('>>>')
+
+def login(file_name, username, hashed_pw):
     line_number = 0
-    list_of_results = []
+    result = []
 
     with open(file_name, 'r') as read_obj:
 
         for line in read_obj:
             line_number += 1
 
-            if string_to_search in line:
-                list_of_results.append((line_number, line.rstrip()))
+            if username in line:
+                result.append((line.rstrip()))
 
-    return list_of_results
-
-def menu():
-    print('1. Login\n2. Sign up\n0. Log Off \n')
+    result = result[0].split()
+    
+    if result[1] == hashed_pw:
+        return 1
+    
+    return 0
 
 while True:
-    menu()
+    chc = MENU()
     while True:
-            chc = input('>>>')
             if chc in ['1','2','0']:
                 break
             print('Choose 1 / 2 / 0')
-    
+            chc = input('>>>')
 
-    if chc == '1':  
-        a = search_string_in_file(path,'Michael')
-        print(a)
-            
+    if chc == '1':
+        path = 'database/user.txt'
+        username = input('Input Username : ')
+        passwd = sha256(input('Input Password : '))
+        
+        if login(path,username, passwd):
+            os.system('cls')
+
+            while True:
+                chc = MENU('MAIN_MENU')
+
+        
+        else:
+            print('Password atau Username Salah')
+
     if chc == '2':
         username = input('Input Username : ')
         password = input('Input Password : ')
         path = 'database/user.txt'
-        print(os.path.exists(path))
+
         if os.path.exists(path):
-            user = username + ' ' + password + '\n'
+            user = username + ' ' + sha256(password) + '\n'
             f = open(path,'a')
             f.write(user)
             f.close()
             
         else:
-            user = username + ' ' + password + '\n'
+            user = username + ' ' + sha256(password) + '\n'
             f = open(path,'a')
             f.write('Username Password\n')
             f.write(user)
---
+++ test.py
@@ -0,0 +1,4 @@
+a = [('jkhadskjhasdhjk 123123312')]
+
+b = a[0].split()
+print(b)
